4ce9500deea3d3a0bca866b06c54ea4c7a677ada
elidaher <eeli56315@gmail.com>
2025-06-15 11:10:46 +0300

    test get

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,6 +4,6 @@ import { login, register } from "../controllers/auth.controller";
 const router = Router();
 
 router.post("/login", login);
-router.post("/register", register);
+router.get("/register", register);
 
 export default router;
