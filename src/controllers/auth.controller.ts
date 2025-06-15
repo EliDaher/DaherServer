5b31ef3362c7555e737a8e9d775e248db7fcb8dd
elidaher <eeli56315@gmail.com>
2025-06-15 12:12:31 +0300

    add login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,15 +1,48 @@
 import { Request, Response } from "express";
-import { generateToken } from "../utils/jwt";
+const {
+  ref,
+  get,
+  query,
+  orderByChild,
+  equalTo,
+} = require("firebase/database");
+const { database } = require("./firebaseConfig.js");
 
 export const login = async (req: Request, res: Response) => {
   const { username, password } = req.body;
 
-  if (username === "admin" && password === "1234") {
-    const token = generateToken({ username });
-    return res.json({ success: true, token });
-  }
+  try {
+    const dbRef = ref(database, "user");
+    const searchQuery = query(
+      dbRef,
+      orderByChild("username"),
+      equalTo(username)
+    );
 
-  return res.status(401).json({ success: false, message: "Invalid credentials" });
+    const snapshot = await get(searchQuery);
+
+    // التحقق مما إذا كان المستخدم موجودًا
+    if (!snapshot.exists()) {
+      return res.status(401).json({ error: "User not found" });
+    }
+
+    const data = snapshot.val();
+    const usersList = Object.keys(data).map((key) => ({
+      id: key,
+      ...data[key],
+    }));
+
+    // التحقق من كلمة المرور
+    const user = usersList.find((user) => user.password === password);
+    if (!user) {
+      return res.status(401).json({ error: "Invalid password" });
+    }
+
+    res.json({ message: "Login successful", user });
+  } catch (error) {
+    console.error("Error Firebase Login: ", error);
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
 };
 
 export const register = (req: Request, res: Response) => {
