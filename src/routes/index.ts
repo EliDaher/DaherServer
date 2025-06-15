660cc09eafcab224e5390b4aeb6a6a2f4e670d7b
elidaher <eeli56315@gmail.com>
2025-06-15 12:40:33 +0300

    get wifi customers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes";
+import wifiRoutes from "./wifi.routes"
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+
+router.use("/wifi", wifiRoutes);
 
 export default router;
