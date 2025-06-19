77be9235ad94c0a4d9942f62ed997fd70b4a728b
elidaher <eeli56315@gmail.com>
2025-06-19 22:47:10 +0300

    up rime robot

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the server!');
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
