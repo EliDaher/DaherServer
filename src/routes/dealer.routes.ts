c07b7ffcc303901aa79a4627ffefbe642ce069ec
elidaher <eeli56315@gmail.com>
2025-07-11 12:20:39 +0300

    addPayment route

--- src/routes/dealer.routes.ts
+++ src/routes/dealer.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import addPayment from "../controllers/dealer.controller";
 
 const router = Router();
 
-
+router.post("/addPayment", addPayment);
 
 export default router;
